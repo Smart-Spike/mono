ea45edf657bf3525930e4162c9ecea96902df3b9
Ben Smith <ben@hellosmithy.com>
2018-07-13 14:16:32 +0100

    Add component generator to cli

--- packages/mono-cli/generators/react-component/index.js
+++ packages/mono-cli/generators/react-component/index.js
@@ -11,8 +11,17 @@ const util = require('util');
 const fs = require('fs');
 const glob = util.promisify(require('glob'));
 const fuzzy = require('fuzzy');
-const pkgJson = require(path.join(process.env.MONO_ROOT, 'package.json'));
+const generateSuggestedTargetPath = require('../utils/generateSuggestedTargetPath');
+
+const rootDir = process.env.MONO_ROOT;
+const pkgJson = require(path.join(rootDir, 'package.json'));
 const pkgRoots = pkgJson.workspaces.packages;
+
+const getTargetPathFromAnswers = (answers) => {
+  return answers.useSuggestedTargetPath
+    ? generateSuggestedTargetPath(answers.targetPackage, 'components')
+    : path.join(rootDir, answers.targetPackage, answers.targetPath);
+};
 
 module.exports = (plop) => {
   plop.inquirer.registerPrompt('autocomplete', require('inquirer-autocomplete-prompt'));
@@ -24,7 +33,7 @@ module.exports = (plop) => {
         name: 'targetPackage',
         message: 'Select a package or project for your component (fuzzy matching)',
         source: (answers, input) => {
-          return glob(`{${pkgRoots.join(',')}}`, { cwd: process.env.MONO_ROOT })
+          return glob(`{${pkgRoots.join(',')}}`, { cwd: rootDir })
             .then(results => {
               return input
                 ? fuzzy.filter(input, results).map(match => match.string)
@@ -33,13 +42,21 @@ module.exports = (plop) => {
         }
       },
       {
+        type: 'confirm',
+        name: 'useSuggestedTargetPath',
+        prefix: 'Create in suggested target directory',
+        message: (answers) => generateSuggestedTargetPath(answers.targetPackage, 'components'),
+        when: (answers) => Boolean(generateSuggestedTargetPath(answers.targetPackage, 'components')),
+        default: true,
+      },
+      {
         type: 'autocomplete',
         name: 'targetPath',
         message: 'Where would you like to create your component?',
-        suggestOnly: true,
+        when: answers => !answers.useSuggestedTargetPath,
         source: (answers, input) => {
           return glob('**/', {
-            cwd: path.join(process.env.MONO_ROOT, answers.targetPackage),
+            cwd: path.join(rootDir, answers.targetPackage),
             ignore: ['node_modules/**', 'coverage/**', 'docs/**', 'dist/**', 'build/**', 'internals/**', 'server/**']
           }).then(results => {
             return input
@@ -64,7 +81,8 @@ module.exports = (plop) => {
         message: 'What should it be called?',
         validate: (value, answers) => {
           if (/.+/.test(value)) {
-            return fs.existsSync(path.join(process.env.MONO_ROOT, answers.targetPackage, answers.targetPath, value))
+            const targetPath = getTargetPathFromAnswers(answers);
+            return fs.existsSync(path.join(targetPath, value))
               ? 'A component or container with this name already exists'
               : true;
           }
@@ -102,13 +120,13 @@ module.exports = (plop) => {
       const actions = [
         {
           type: 'add',
-          path: `${path.join(process.env.MONO_ROOT, data.targetPackage, data.targetPath)}/{{properCase name}}/index.tsx`,
+          path: `${getTargetPathFromAnswers(data)}/{{properCase name}}/index.tsx`,
           templateFile: componentTemplate,
           abortOnFail: true,
         },
         {
           type: 'add',
-          path: `${path.join(process.env.MONO_ROOT, data.targetPackage, data.targetPath)}/{{properCase name}}/tests/index.test.tsx`,
+          path: `${getTargetPathFromAnswers(data)}/{{properCase name}}/tests/index.test.tsx`,
           templateFile: './react-component/test.tsx.hbs',
           abortOnFail: true,
         },
@@ -118,7 +136,7 @@ module.exports = (plop) => {
       if (data.wantMessages) {
         actions.push({
           type: 'add',
-          path: `${path.join(process.env.MONO_ROOT, data.targetPackage, data.targetPath)}/{{properCase name}}/messages.ts`,
+          path: `${getTargetPathFromAnswers(data)}/{{properCase name}}/messages.ts`,
           templateFile: './react-component/messages.ts.hbs',
           abortOnFail: true,
         });
@@ -128,7 +146,7 @@ module.exports = (plop) => {
       if (data.wantLoadable) {
         actions.push({
           type: 'add',
-          path: `${path.join(process.env.MONO_ROOT, data.targetPackage, data.targetPath)}/{{properCase name}}/Loadable.ts`,
+          path: `${getTargetPathFromAnswers(data)}/{{properCase name}}/Loadable.ts`,
           templateFile: './react-component/loadable.ts.hbs',
           abortOnFail: true,
         });
